d4552dea8e627f05bdd62f4061624920930fdd24
Tymoteusz Szturc <szturc11tymek@gmail.com>
2020-12-17 17:37:58 +0100

    Init commit, login page

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const  express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+
+
+
+app.listen(port, () => {
+    console.log('Server is up on port' + port);
+})
