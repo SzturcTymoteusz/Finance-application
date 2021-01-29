6d1824e8bf468476b84d058ed6fc647424bb0f4f
Tymoteusz Szturc <szturc11tymek@gmail.com>
2021-01-29 19:06:29 +0100

    Add window EditProfile

---
+++ public/js/user/user.js
@@ -0,0 +1,17 @@
+const btnOpenExpenses = [...document.querySelectorAll('.content__option')][0];
+const btnOpenEditProfile = [...document.querySelectorAll('.content__option')][1];
+const windowExpenses = document.querySelector('.expenses');
+const windowEditProfile = document.querySelector('.edit-profile');
+
+
+btnOpenExpenses.addEventListener('click', () => {
+    windowExpenses.classList.remove('expense--hidden');
+    windowEditProfile.classList.add('edit-profile--hidden');
+});
+
+btnOpenEditProfile.addEventListener('click', () => {
+    windowExpenses.classList.add('expense--hidden');
+    windowEditProfile.classList.remove('edit-profile--hidden');
+})
+console.log(windowExpenses);
+console.log(windowEditProfile);
